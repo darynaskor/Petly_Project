005a874723fb233c01ace1097e33e0b3ea3386eb
Daryna Skorobohatykh <146958283+darynaskor@users.noreply.github.com>
2025-10-27 16:43:23 +0200

    wrong file pushed

--- GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Petly.Maui")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Petly.Maui.Pages")]
