a9ac3e32f238baa909dd771acb71baa78c92b16d
haroqub <haroqub@gmail.com>
2025-12-01 11:54:38 +0200

    Add files via upload

--- AnimalShelter.BLL/Services/ShelterService.cs
+++ AnimalShelter.BLL/Services/ShelterService.cs
@@ -7,17 +7,15 @@ namespace AnimalShelter.BLL.Services
     {
         private readonly GenericRepository<Shelter> _repository;
 
-
         public ShelterService(GenericRepository<Shelter> repository)
         {
             _repository = repository;
         }
 
-
-        public async Task<List<Shelter>> GetAllAsync() => await _repository.GetAllAsync();
-        public async Task<Shelter?> GetByIdAsync(int id) => await _repository.GetByIdAsync(id);
-        public async Task AddAsync(Shelter shelter) => await _repository.AddAsync(shelter);
-        public async Task UpdateAsync(Shelter shelter) => await _repository.UpdateAsync(shelter);
-        public async Task DeleteAsync(int id) => await _repository.DeleteAsync(id);
+        public async Task<List<Shelter>> GetAllShelterAsync() => await _repository.GetAllAsync();
+        public async Task<Shelter?> GetShelterByIdAsync(int id) => await _repository.GetByIdAsync(id);
+        public async Task AddShelterAsync(Shelter shelter) => await _repository.AddAsync(shelter);
+        public async Task UpdateShelterAsync(Shelter shelter) => await _repository.UpdateAsync(shelter);
+        public async Task DeleteShelterAsync(int id) => await _repository.DeleteAsync(id);
     }
 }
--- AnimalShelter.BLL/Services/UserService.cs
+++ AnimalShelter.BLL/Services/UserService.cs
@@ -7,17 +7,15 @@ namespace AnimalShelter.BLL.Services
     {
         private readonly GenericRepository<User> _repository;
 
-
         public UserService(GenericRepository<User> repository)
         {
             _repository = repository;
         }
 
-
-        public async Task<List<User>> GetAllAsync() => await _repository.GetAllAsync();
-        public async Task<User?> GetByIdAsync(int id) => await _repository.GetByIdAsync(id);
-        public async Task AddAsync(User user) => await _repository.AddAsync(user);
-        public async Task UpdateAsync(User user) => await _repository.UpdateAsync(user);
-        public async Task DeleteAsync(int id) => await _repository.DeleteAsync(id);
+        public async Task<List<User>> GetAllUsersAsync() => await _repository.GetAllAsync();
+        public async Task<User?> GetUserByIdAsync(int id) => await _repository.GetByIdAsync(id);
+        public async Task AddUserAsync(User user) => await _repository.AddAsync(user);
+        public async Task UpdateUserAsync(User user) => await _repository.UpdateAsync(user);
+        public async Task DeleteUserAsync(int id) => await _repository.DeleteAsync(id);
     }
 }
